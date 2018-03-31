861b105cc4b7c9cc7932ab01e091ae3e31527ecd
alexey novikov <alexey.novikov@vimcar.com>
2018-03-31 12:51:23 +0200

    Protocols proposal + dataSource sample implementation

---
+++ CodeChallenge/Protocols/Proposal/RepositoriesDataProvider.h
@@ -0,0 +1,13 @@
+//
+//  RepositoriesDataProvider.h
+//  CodeChallenge
+//
+//  Created by alexey novikov on 31.03.2018.
+//
+
+#import <Foundation/Foundation.h>
+#import "DataProviderProtocol.h"
+
+@interface RepositoriesDataProvider : NSObject<DataProviderProtocol>
+
+@end
